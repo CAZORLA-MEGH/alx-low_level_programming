2a2dd29ed361422511489e8a0c85d64f977e75d7
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-10 08:12:12 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-// main function - // where the execution of program begins
+// main function - 
+// where the execution of program begins
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return 0;}

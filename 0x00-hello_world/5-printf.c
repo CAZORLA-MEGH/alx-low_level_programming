2d6b2eb8173b3b5a14fb43c17d2cc4a3671270ea
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-10 11:34:46 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - start point of the program
+ *
+ *Return: always 0 its (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+

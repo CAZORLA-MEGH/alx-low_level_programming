2274cb74c2994388eaeaa4545ddf7d2ff0f02e74
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 15:10:35 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+/**
+ * main - start point of the program
+ * Return: always 0 its (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
+

304f10abc3d50bb2c818f1e4e4f507699a14d012
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 10:57:56 -0700

    1. The last digit

---
+++ 0x00-hello_world/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - start point of the program
+ * Return: always 0 its (success)
+*/
+int main(void)
+{
+int n, digit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+digit = n %10
+if ( digit > 5 )
+{
+printf ("last digit of %d is %d and is great than 5", n , digit);
+}
+if ( digit == 0 )
+{
+printf ("last digit of %d is %d and is 0", n, digit);
+}
+if ( digit > 6 && digit != 0 )
+{
+printf ("last digit of %d is %d and is less than 6 and not 0", n, digit);
+}
+return (0);
+}

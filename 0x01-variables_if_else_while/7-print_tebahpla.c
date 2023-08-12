6e12ffd132c5b26c982211ff54fb6a845ddeb548
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 18:47:40 -0700

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,6 +3,8 @@
 /**
  * main - start point of the program
  * Return: always 0 its (success)
+ * reverse: Function to print reverse of
+ * the passed string
   */
 
 void reverse(char *str)
@@ -15,7 +17,7 @@ printf("%c", *str);
 }
 int main()
 {
-char a[] = "\nabcdfghijklmnoprstuvwxyz";
+char a[] = "\nabcdefghijklmnopqrstuvwxyz";
 reverse(a);
 return 0;
 }

f18b34a5dc5a8ed905bfc864aa8e536bee7a3876
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 19:56:47 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  *  * main - start point of the program
+ *   *   * Return: always 0 its (success)
+ *    *    */
+
+#include <stdio.h>
+int main(void)
+{
+int i;
+for (i = 0; i <= 8; i++)
+{ printf(" %x,", i);
+}
+printf(" 9 $");
+
+
+return (0);
+}

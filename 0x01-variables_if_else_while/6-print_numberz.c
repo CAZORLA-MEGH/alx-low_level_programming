e19e184cd3f901286a71cd2ebcf7c4f6f2046861
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 17:59:39 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - start point of the program
+ * Return: always 0 its (success)
+ */
+int main(void)
+{
+int counter = 10;
+
+for (int num = 0; num <= counter; num++)
+{
+putchar(num + '0')
+}
+putchar('\n')
+return (0);
+}

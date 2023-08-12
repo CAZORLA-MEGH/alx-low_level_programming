a189922208372480178b5f71209649f64749bc18
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 19:10:55 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start point of the program
+ * Return: always 0 its (success)
+ */
+
+int main()
+{
+int i;
+for (i = 1; i <= 15; i++)
+{ printf("%x ", i); }
+return 0;
+}

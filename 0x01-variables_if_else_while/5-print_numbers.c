5a05ace5cb005bd4b189fe336029363fbf2765fb
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-11 17:12:30 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *  *  * main - start point of the program
- *   *   * Return: always 0 its (success)
- *    *    */
+ * main - start point of the program
+ *Return: always 0 its (success)
+ */
 int main(void)
 {
 int x;
 x = 0;
 while(x<10)
 {
-printf("%d,x");
+printf("%d", x);
 x++;
 }
 return (0);

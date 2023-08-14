a8b23e3347da91aa1f0877af126d77f6536da755
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-14 16:03:34 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+/**
+ * main - start point of the program
+ * Return: always 0 its (success)
+ */
+
+
+#include <stdio.h>
+int main(void)
+{
+int i, j, z, k;
+for(i = 0; i <= 9; i++)
+{
+for(j = 0; j <= 9; j++)
+{
+for(z = 0; z <= 9; z++)
+{
+for(k = 0; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(z + '0');
+putchar(k + '0');
+putchar(' ');
+{
+putchar(' ');
+putchar(',');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

d5a28fb4bfec910a0c4f1e6e6752049e65c74e35
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-15 11:18:06 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+void print_alphabet(void)
+{
+	int alpha;
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	{putchar(alpha);
+	}
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+
+}
+

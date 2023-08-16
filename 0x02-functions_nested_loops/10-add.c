f3bb611b01f1e9609c63c1028651851dc8d44343
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-16 06:36:45 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * main - start point of the program
+ * Description :
+ * Return: always 0 its (success)
+ */
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+int add(int x, int z)
+{
+return x + z;
+}
+

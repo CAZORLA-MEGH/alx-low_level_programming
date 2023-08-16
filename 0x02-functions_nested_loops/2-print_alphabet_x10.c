23eda531eddd4f3c1540d8eaedfcc1d83ca68bb8
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-15 19:31:35 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+/**
+ * main - start point of the program
+ * function print_alphabet -  prints the alphabet, in lowercase
+ * Return: always 0 its (success)
+ */
+
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+void print_alphabet_x10(void)
+{
+char letter;
+int j;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if( letter == 'z'){
+for (j = 1; j <= 26; j++)
+{
+
+
+_putchar(letter);
+}
+}
+}
+_putchar('\n');
+}
+
+
+
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+ * main - start point of the program
+ * function print_alphabet -  prints the alphabet, in lowercase
+ * Return: always 0 its (success)
+ */
+
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+int _islower(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+return (1);
+else 
+return(0);
+}
+

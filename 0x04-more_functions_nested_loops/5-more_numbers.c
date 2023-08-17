57fcc1899316715c30b84f823b913fb292fe246e
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-17 14:42:07 -0700

    prints 10 times the numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+/**
+ *
+ *
+ *
+ */
+#include <stdio.h>
+void more_numbers(void)
+{
+int a, z, e;
+for (a = 0; a <= 9; a++)
+putchar(a);
+{
+for (z = 1; z <= 4; z++)
+{
+for (e = 0; e <= 4; e++)
+
+{
+if (a == '9')
+putchar(z + );
+putchar(e + );
+}
+}
+}
+}
+
+

3765b2d14009044b4e5e086967991932780343a6
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-17 19:17:06 -0700

    7. I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+/**
+ * print_diagonal -
+ * @n - number
+ * @z -number
+ * return
+*/
+#include<stdio.h>
+void print_diagonal(int n)
+{
+int z;
+{
+for (z = 0; z < n; z++)
+{
+putchar('\\');
+if (n <= 0)
+putchar('\n');
+}
+}
+putchar('\n');
+}

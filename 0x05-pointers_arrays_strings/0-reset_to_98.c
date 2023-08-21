7785e29fe711aa74b28aaa4028a99c823c42d58d
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-21 11:52:50 -0700

    1. Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+
+
+
+
+
+#include<stdio.h>
+#include"main.h"
+void reset_to_98(int *n)
+{
+
+
+int p ; 
+n = &p;
+*p = 98; 
+
+
+}
+
+
+
+
+
+
+
+
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int : function that swaps the values of two integers.
+ * @a: num 1
+ * @b: num 2
+ *
+*/
+
+
+
+#include <stdio.h>
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}

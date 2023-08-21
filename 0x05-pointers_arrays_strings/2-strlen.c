4598f9aec9ac9c218ba9f90279f2bcf3e394eca3
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-21 15:06:31 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+/**
+ *_strlen - function that returns the length of a string.
+ * @s: contains 16 character
+ * @len - Number Character
+ * Return: len
+*/
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+int _strlen(char *s)
+{
+int len;
+s = "My first strlen!";
+len = strlen(s);
+return (len);
+
+
+}

94f020fc07872011b722b0753f2760d2d25dcec1
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-22 16:26:58 -0700

    9.strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * *_strcpy - copies the string
+ * @src 
+ * @dest
+ * 
+ */
+
+
+
+
+
+
+#include<stdio.h>
+#include <string.h>
+char *_strcpy(char *dest, char *src)
+{
+strcpy(src, dest);
+printf("%s, src");
+return (dest);
+}

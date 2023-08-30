54b53bd99925d95c86817367681fb759c5ddf6a9
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-08-30 15:07:31 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _puts_recursion -  that prints a stringthat prints a string
+ *  @s : string
+ */
+
+#include<stdio.h>
+
+
+void _puts_recursion(char *s)
+{
+puts(s);
+puts('\n');
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int _sqrt_recursion(int n)
+
+{
+if (n!sqrt)
+return (-1);
+else
+return (n * _sqrt_recursion(n - 1);
+	}

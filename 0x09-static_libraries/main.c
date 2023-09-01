ef656c4054b3bce07e9e48d902408038e54f9e7f
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-09-01 14:49:41 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,14 @@
+/**
+ *main - main function
+ *Return: always 0
+ */
+
 #include "main.h"
 
 int main(void)
 {
 
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	        return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker");
+_puts("\"\n\t- Kevin Mitnick");
+return (0);
 }

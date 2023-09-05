7636741e6db19c4e13e9a22eb3d88e4ca560d08c
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-09-05 16:36:34 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,35 +6,36 @@
  *@ptr -duplicateString
  * Return: return NULL if input NULL , return ptr if successful
  */
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
 char *_strdup(char *str)
 {
-int i = 0;
-int size = 0;
+int i;
 char *ptr;
-
+int size;
+if (str == NULL)
+{
+return (NULL);
+}
+size = 0;
 while (str[size] != '\0')
 {
-	size++;
+size++;
 }
 ptr = (char *) malloc(size + 1);
 
 if (ptr == NULL)
 {
+printf("failed to allocate memory\n");
 return (NULL);
 }
-
+i = 0;
 while (str[i] != '\0')
 {
 ptr[i] = str[i];
 i++;
 }
-ptr[size] = '\0';
+ptr[i] = '\0';
 return (ptr);
 }

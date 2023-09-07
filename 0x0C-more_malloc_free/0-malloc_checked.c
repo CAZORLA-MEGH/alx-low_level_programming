a098ceb7fb90e57edfaedcad4babb34474cc9c63
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-09-07 11:25:28 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 void *malloc_checked(unsigned int b)
 
 {
-
-void *ptr = malloc (b * sizeof(int));
-if( ptr == NULL){
-printf(stderr, "malloc failed\n");
+void *ptr;
+ptr = malloc (b);
+if (ptr == NULL)
 
 exit(98);
-}
-return ptr;
+
+return (ptr);
 }

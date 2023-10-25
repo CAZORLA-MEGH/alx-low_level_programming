076435704269368a3dcf9c00a530f1023f114c05
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-10-25 01:56:15 -0700

    error: pathspec 'She' did not match any file(s) known to git

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+/**
+* _puts_recursion -  is a recursive function that prints a string character,
+*  @s: which is a pointer to the beginning of the string to be printed.
+*
+*/
+
+
+#include <stdio.h>
+#include "main.h"
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+
+}
+
+

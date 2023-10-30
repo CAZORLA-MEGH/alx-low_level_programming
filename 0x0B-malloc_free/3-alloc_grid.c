7f43dcbc7e0136ad107a359a60fc0efe104da194
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-10-30 15:55:51 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,36 +13,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-int **alloc_grid(int width, int height) {
+int **alloc_grid(int width, int height)
+{
 int i, j;
 int **ptr;
-
-if (width <= 0 || height <= 0) {
-return NULL; 
-}
-
 ptr = (int **)malloc(width * sizeof(int *));
-if (ptr == NULL) {
-return NULL; 
+if (width <= 0 || height <= 0)
+{
+return (NULL);
 }
-
-for (i = 0; i < width; i++) {
-ptr[i] = (int *)malloc(height * sizeof(int));
-if (ptr[i] == NULL) {
-for (j = 0; j < i; j++) {
-free(ptr[j]);
+if (ptr == NULL)
+{
+return (NULL);
 }
-free(ptr);
-return NULL;
+for (i = 0; i < height; i++)
+{
+ptr[i] = (int *)malloc (width * sizeof(int));
+if (ptr[i] == NULL)
+return (NULL);
 }
-}
-
-for (i = 0; i < width; i++) {
-for (j = 0; j < height; j++) {
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
 ptr[i][j] = 0;
 }
-}
-
-return ptr;
+return (ptr);
 }
 

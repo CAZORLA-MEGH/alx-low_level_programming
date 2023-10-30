cff444aefe6283dcb7b67b9b7f5fd33ae088b6c5
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-10-30 09:30:05 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+/**
+*_strdup - returns a pointer to a new string which is aduplicate of str
+*  @str:  pointer to a null-terminated string that you want to duplicate.
+*  @s - pointer to (char)and is used to store the DA duplicate string.
+*  @lenght - calculates the length of the input string str
+*@i - used as a loop  for iterating through the characters in the input string.
+* Return: pointer *s.
+*/
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+int i;
+char *s;
+int lenght = strlen(str);
+if (str == NULL)
+{
+return (NULL);
+}
+s = (char *) malloc((lenght) * sizeof(char));
+for (i = 0; str[i] != '\0'; i++)
+{
+s[i] = str[i];
+}
+s[i] = '\0';
+
+return (s);
+}

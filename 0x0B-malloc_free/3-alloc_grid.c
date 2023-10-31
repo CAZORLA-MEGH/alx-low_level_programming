c312b3f9419f3286b116e67175a16257c8fb163b
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-10-31 14:44:08 -0700

    3grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,8 +30,12 @@ return (NULL);
 for (i = 0; i < height; i++)
 {
 ptr[i] = malloc(width * sizeof(int));
-if (ptr[i] == NULL)
+if (ptr[i] == NULL){
+for (i--; i >= 0; i--)
+free(ptr[i]);
+free(ptr);
 return (NULL);
+}
 }
 for (i = 0; i < height; i++)
 {

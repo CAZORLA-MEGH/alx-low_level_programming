17acc03b9c21aeaf2fd1c525b64c1aeb26023724
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-02 03:48:22 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,20 +14,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i, j;
 unsigned int size_s1 = 0;
 unsigned int size_s2 = 0;
+unsigned int size_s3 = 0;
+unsigned int num = n;
 char *s3;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (s1 && s1[size_s1])
+for (i = 0; s1[i] != '\0'; i++)
 size_s1++;
-while (s2 && s2[size_s2])
+for (i = 0; s2[i] != '\0'; i++)
 size_s2++;
-
-if (n < size_s2)
-s3 = malloc(sizeof(char) * (size_s1 + n + 1));
-else
-s3 = malloc(sizeof(char) * (size_s1 + size_s2 + 1));
+if (n >= size_s2)
+num = size_s2;
+size_s3 = size_s1 + num + 1;
+s3 = malloc(sizeof(char) * (size_s3));
 
 if (s3 == NULL)
 return (NULL);
@@ -35,11 +36,13 @@ for (i = 0; s1[i] != '\0'; i++)
 {
 s3[i] = s1[i];
 }
-for (j = 0; j <= n; j++)
+for (j = 0; j < num; j++)
 {
 s3[i + j] = s2[j];
 }
 s3[i + j] = '\0';
 return (s3);
 }
+
+
 

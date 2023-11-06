d1859f7748540ae69cd51d7512358cd37fb08f87
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-06 08:45:48 -0800

    preproc

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+/**
+* main - The primary function in the C program.
+* @argc:  number of command-line arguments
+ * @argv: array of string
+* Return: int
+*/
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+argc = 0;
+printf("%s\n", argv[argc]);
+return (0);
+}

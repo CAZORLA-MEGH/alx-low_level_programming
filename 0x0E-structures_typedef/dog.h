37916605c17068d8e757284a60b19e646f8e4484
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-06 14:58:50 -0800

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,19 +12,9 @@ char *name;
 float age;
 char *owner;
 };
-/**
- * struct dog_t - a dog's basic info
- * @name: variable name
- * @age:variable age
- * @owner:variable owner
- */
-struct dog_t
-{
-char *name;
-float age;
-char *owner;
-};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif /* DOG_H */

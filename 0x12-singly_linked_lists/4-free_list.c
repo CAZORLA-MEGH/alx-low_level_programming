2bdd7883be624eaa117f50c526c29bfd6c55f1cb
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-16 15:56:04 -0800

    4-free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+/**
+ *free_list - fress a list_t.
+ *@head : pointer to head of list.
+ */
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+list_t *temp;
+while (!head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}

4febdaecbc1659fdc47dfe1edf5d387cb741c07d
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-27 15:54:18 -0800

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,8 +7,8 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int sz = 0, i = 0;
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+int wr = 0, i = 0;
+int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 if (filename == NULL || text_content == NULL)
 return (0);
 if (fd == -1)
@@ -16,8 +16,8 @@ if (fd == -1)
 return (0);
 }
 for (i = 0; text_content[i] != '\0'; i++)
-sz = write(fd, text_content, i);
-if (sz == -1 || fd == -1)
+wr = write(fd, text_content, i);
+if (wr == -1 || fd == -1)
 {
 return (0);
 }

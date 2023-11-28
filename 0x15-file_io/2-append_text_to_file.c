34dbc9057247278a175c9da4c66549365065db52
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-27 18:11:00 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, append, i;
-if (text_conten == NULL)
+if (text_content == NULL)
 return (-1);
 if (filename == NULL)
 return (-1);

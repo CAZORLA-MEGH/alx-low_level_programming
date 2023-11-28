bd384b8e66ab0d8ef8fcd98bf57f5e3c8b43022c
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-27 17:43:14 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ if (file1 == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 664);
+file2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 if (file2 == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);

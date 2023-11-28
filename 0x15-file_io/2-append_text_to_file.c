e7a8f8bd89a05447f3fcf2bb85753e997ff95556
CAZORLA-MEGH <amineelmeghraoui47@gmail.com>
2023-11-27 16:42:47 -0800

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename: pointer to nam eof file.
+* @text_content: null terminated string.
+*
+* Return: 1 on success, -1 on failure.
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, append, i;
+
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_EXCL | O_APPEND);
+if (file < 0)
+return (-1);
+
+if (text_content)
+{
+for (i = 0; text_content[i] != '\0'; i++)
+append = write(file, text_content, i);
+if (append < 0)
+{
+close(file);
+return (-1);
+}
+}
+close(file);
+return (1);
+}
+
